a6725b5201c87d840a80a159bbc801dcc40484e5
Plamen Draganov <159624750+PlamenDraganov16@users.noreply.github.com>
2025-05-28 20:00:05 +0300

    Delete ADVANCED/DOM Introduction/catchmouse directory

--- ADVANCED/DOM Introduction/catchmouse/01.Edit Element/EditElement.js
+++
@@ -1,5 +0,0 @@
-function editElement(ref, match, replacer) {
-    let textContent = ref.textContent;
-    let result = (textContent.replace(new RegExp(match, 'g'), replacer));
-    ref.textContent = result;
-}
