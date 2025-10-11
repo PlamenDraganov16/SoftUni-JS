2580a6579b47a2155fa10430b659f5321e7179e6
Plamen Draganov <159624750+PlamenDraganov16@users.noreply.github.com>
2025-10-11 17:10:39 +0300

    how to use bcrypt and authenticate with salt

--- JSBACKEND/Session and Authentication/index.js
+++ JSBACKEND/Session and Authentication/index.js
@@ -1,10 +1,21 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import expressSession from 'express-session';
+import bcrypt from 'bcrypt';
+
+const users = [];
 
 const app = express();
 
 // Middleware
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(expressSession({
+    secret: '1234567890',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}));
 
 app.get('/', (req, res) => {
     res.send('Works.');
@@ -12,7 +23,7 @@ app.get('/', (req, res) => {
 
 // HTTP Module set cookie method
 app.get('/set-cookie', (req, res) => {
-    
+
     res.writeHead(200, {
         'set-cookie': 'name=Pesho',
     });
@@ -53,5 +64,100 @@ app.get('/get-cookie2', (req, res) => {
 
     res.end;
 });
+
+// Set session using express-session library
+app.get('/set-session', (req, res) => {
+    req.session.name = 'Gosho';
+    req.session.age = 20;
+
+    res.send('Session is created');
+})
+
+app.get('/get-session', (req, res) => {
+    res.send(req.session.name);
+})
+
+// Register 
+
+app.get('/register', (req, res) => {
+    res.send(`
+        <form method="post">
+            <div>
+                <label for="username">Username</label>
+                <input type="text" id="username" name="username" />
+            </div>
+            <div>
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password" />
+            </div>
+            <div>
+                <input type="submit" value="Register">
+            </div>
+        </form>
+    `);
+});
+
+app.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+
+    // generate salt
+    const salt = await bcrypt.genSalt(10);
+
+    // Hash password
+    const hash = await bcrypt.hash(password, salt);
+
+    const user = {
+        id: users.length,
+        username,
+        password: hash
+    };
+
+    users.push(user);
+
+    res.redirect('/login');
+});
+
+// Login
+
+app.get('/login', (req, res) => {
+
+    res.send(`
+        <form method="post">
+            <div>
+                <label for="username">Username</label>
+                <input type="text" id="username" name="username" />
+            </div>
+            <div>
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password" />
+            </div>
+            <div>
+                <input type="submit" value="Login">
+            </div>
+        </form>
+    `);
+});
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    // Take user
+    const user = users.find(u => u.username === username);
+
+    if (!user) return res.send('No such user!');
+
+    // Compare passwords
+
+    const isPassValid = await bcrypt.compare(password, user.password);
+
+    if (!isPassValid) return res.send('Invalid password!');
+
+    res.send(`Welcome ${username}`);
+    // res.redirect('/profile');
+});
+
+app.get('/profile', (req, res) => {
+    res.send(`Welcome ${username}`);
+})
 
 app.listen(5000, () => console.log('Server is listening on http://localhost:5000'));
